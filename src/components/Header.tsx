c83f40e99b06b15899bc089745858dda3dc12aa1
mohamedfetoh <mohammedragab44445@gmail.com>
2026-07-09 00:27:28 +0300

    Edited

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="h-[800px] bg-[url(./img/download.jpg)] bg-cover bg-center flex items-center  ">
+    <header className="h-[800px] header bg-cover bg-center flex items-center  ">
       <h2 className="md:ml-25 p-10 bg-[#58a28a] text-white text-4xl w-[450px] rounded">
         Always laugh <br /> when you can,
         <br />
